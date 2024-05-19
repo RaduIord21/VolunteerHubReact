0af93e2eca72d5703526deff11e23e93151a2be5
Rayder 2021 <nemus20@gmail.com>
2024-05-19 12:23:37 +0300

    quit org

---
+++ src/Components/SelectNewCoordinatorPopup.jsx
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import {Modal, Button, Form} from 'react-bootstrap';
+
+const SelectNewCoordinatorPopup = ({currentUser, users, show, handleClose, onUserSelect}) => {
+    const [selectedUser, setSelectedUser] = useState('');
+
+    const handleChange = (event) => {
+        const selectedUserId = event.target.value;
+        setSelectedUser(selectedUserId);
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        onUserSelect(selectedUser);
+        handleClose();
+    };
+
+    return (
+        <Modal show={show} onHide={handleClose} animation={false}>
+            <Modal.Header closeButton>
+                <Modal.Title>Selectare coordonator nou</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <p>Voluntarul selectat din lista va deveni noul coordonator al organizatiei</p>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group controlId="userSelect">
+                        <Form.Label>Membri</Form.Label>
+                        <Form.Control as="select" value={selectedUser} onChange={handleChange}>
+                            <option value="" disabled>Selecteaza..</option>
+                            {users.filter((user) => user.userName !== currentUser).map((user) => (
+                                <option key={user.userName} value={user.userName}>
+                                    {user.userName}
+                                </option>
+                            ))}
+                        </Form.Control>
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Renunta
+                </Button>
+                <Button variant="primary" onClick={handleSubmit}>
+                    Salveaza
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+export default SelectNewCoordinatorPopup;
--- src/Pages/Organization/MyOrganization.jsx
+++ src/Pages/Organization/MyOrganization.jsx
@@ -1,124 +1,143 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import LoadingSpinner from '../../Components/LoadingSpinner';
 import useAxios from "../../Hooks/useAxios";
-import { format } from 'date-fns';
+import {format} from 'date-fns';
 import {useAuth} from "../../Hooks/AuthProvider";
+import {useNavigate} from "react-router-dom";
+import SelectNewCoordinatorPopup from "../../Components/SelectNewCoordinatorPopup";
 
 function MyOrganization(props) {
 
-  const [organization, setOrganization] = useState("");
-  const [user, setUser] = useState("");
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [role, setRole] = useState("");
-  const axiosInstance = useAxios();
-  const auth = useAuth();
+    const [organization, setOrganization] = useState("");
+    const [user, setUser] = useState("");
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [role, setRole] = useState("");
+    const axiosInstance = useAxios();
+    const auth = useAuth();
+    const [message, setMessage] = useState('');
+    const navigate = useNavigate();
+    const orgId = auth.organizationId;
+    const [showPopup, setShowPopup] = useState(false);
 
-  const orgId = localStorage.getItem("organizationId");
-
-  const handleKick = (email) =>{
-    const data = {
-      email : email
-    }
-    axiosInstance.post('/kick', data
-    ).then(response =>{
-      console.log(response);
-      setUsers(prevUsers => prevUsers.filter(user => user.email !== email));
-    }).catch(error =>{
-      console.error(error);
-    })
-  }
-  useEffect(() => {
-    axiosInstance.get(`/organization/${orgId}/organization`)
-      .then(response => {
-      setOrganization(response.data);
-      setLoading(false);
-      if (response.data === null || response.data === undefined) {
-        console.log("Nu e organizatie");
-      }
-    }).catch(error => {
-      console.log("Eroare fatalaa " + error)
-    })
-
-    axiosInstance.get(`/organization/${orgId}/organization-users`).then(
-      response =>{
-        console.log(response.data);
-        setUsers(response.data);
-      }
-    )
-  }, []);
-
-
-
-
-  if (loading) {
-    return <LoadingSpinner />;
-  }
-
-  const handleQuit = () => {
-    const response = {
-      user: user
-    }
-    axiosInstance.post('/quitOrganization', response
-    ).then(response => {
-        // Handle successful response
-        console.log(user);
-        console.log('Response for success:', response);
-        if (response.data.currentUser === null || response.data.currentUser === undefined) {
+    const handleKick = (email) => {
+        const data = {
+            email: email
         }
-      })
-      .catch(error => {
-        // Handle error
-        console.error('Error: no user ', error);
-      });
-  }
+        axiosInstance.post('/kick', data
+        ).then(response => {
+            console.log(response);
+            setUsers(prevUsers => prevUsers.filter(user => user.email !== email));
+        }).catch(error => {
+            console.error(error);
+        })
+    }
+    useEffect(() => {
+        axiosInstance.get(`/organization/${orgId}/organization`)
+            .then(response => {
+                setOrganization(response.data);
+                setLoading(false);
+                if (response.data === null || response.data === undefined) {
+                    console.log("Nu e organizatie");
+                }
+            }).catch(error => {
+            console.log("Eroare fatalaa " + error)
+        })
+
+        axiosInstance.get(`/organization/${orgId}/organization-users`).then(
+            response => {
+                console.log(response.data);
+                setUsers(response.data);
+            }
+        )
+    }, []);
+
+    if (loading) {
+        return <LoadingSpinner/>;
+    }
+
+    const handleQuit = () => {
+        if (auth.role === 'coordinator') {
+            setShowPopup(true)
+        } else {
+            sendQuit('');
+        }
+
+    }
+
+    const handleClosePopup = () => {
+        setShowPopup(false);
+    }
+
+    const sendQuit = (userName) => {
+        const data = {
+            user: userName
+        }
+        axiosInstance.post('/organization/' + auth.organizationId + '/quitOrganization', data
+        ).then(response => {
+            console.log('response quit', response);
+            auth.updateRole('anonymous');
+            navigate('/dashboard');
+        }).catch(error => {
+            // Handle error
+            console.error('Error: no user ', error);
+        });
+    }
 
 
-  return (
-      <>
-        <h1>Organizatia Mea</h1>
+    return (
+        <>
+            <h1>Organizatia Mea</h1>
+            <SelectNewCoordinatorPopup
+                currentUser={auth.user}
+                show={showPopup}
+                users={users}
+                handleClose={handleClosePopup}
+                onUserSelect={sendQuit}/>
 
-        <div className="card">
-          <div className="card-body">
-            <h4 className="card-title">{organization.name}</h4>
-            <h6 className="card-subtitle mb-2 text-body-secondary">Cod  invitatie: {organization.code}</h6>
-            <p className="card-text"><label>Adresa: </label> {organization.adress ? organization.adress : '-' }</p>
-            <small>Creata: {format(new Date(organization.createdAt), 'dd.MM.yyyy')}</small>
-          </div>
-          <div className="card-footer">
-            <button className='btn btn-danger' onClick={handleQuit}>Quit organization</button>
-          </div>
-        </div>
+            <div className="card">
+                <div className="card-body">
+                    <h4 className="card-title">{organization.name}</h4>
+                    <h6 className="card-subtitle mb-2 text-body-secondary">Cod invitatie: {organization.code}</h6>
+                    <p className="card-text"><label>Adresa: </label> {organization.adress ? organization.adress : '-'}
+                    </p>
+                    <small>Creata: {format(new Date(organization.createdAt), 'dd.MM.yyyy')}</small>
+                </div>
+                <div className="card-footer">
+                    <button className='btn btn-danger' onClick={handleQuit}>Quit organization</button>
+                    {message && <p className="text-danger">{message}</p>}
+                </div>
+            </div>
 
-        <h2></h2>
-        <table className='table'>
-          <thead>
-          <tr>
-            <th scope='col'>Nume</th>
-            <th scope='col'>Email</th>
-            <th scope='col'>Rol</th>
-            <th scope='col'>Actiuni</th>
-          </tr>
-          </thead>
-          <tbody>
-          {users.map((item, index) => (
-              <tr key={index}>
-                <td>{item.userName}</td>
-                <td>{item.email}</td>
-                <td>{item.roles[0]}</td>
-                <td>
-                  {(item.roles[0] === "Coordinator" && item.userName !== auth.user) &&
-                      <button className='btn btn-danger' onClick={() => {
-                        handleKick(item.email)
-                      }}>Kick</button>}
-                </td>
-              </tr>
-          ))
-          }
-          </tbody>
-        </table>
-      </>
-  )
+            <h2></h2>
+            <table className='table'>
+                <thead>
+                <tr>
+                    <th scope='col'>Nume</th>
+                    <th scope='col'>Email</th>
+                    <th scope='col'>Rol</th>
+                    <th scope='col'>Actiuni</th>
+                </tr>
+                </thead>
+                <tbody>
+                {users.map((item, index) => (
+                    <tr key={index}>
+                        <td>{item.userName}</td>
+                        <td>{item.email}</td>
+                        <td>{item.roles[0]}</td>
+                        <td>
+                            {(item.roles[0] === "Coordinator" && item.userName !== auth.user) &&
+                                <button className='btn btn-danger' onClick={() => {
+                                    handleKick(item.email)
+                                }}>Kick</button>}
+                        </td>
+                    </tr>
+                ))
+                }
+                </tbody>
+            </table>
+        </>
+    )
 }
 
 export default MyOrganization;
